0db9984a81562623a0d88c7b8aebf4224f614d05
kinghy <470527375@qq.com>
2025-02-01 19:44:57 +0800

    init front

---
+++ front/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hello World</h1>
+  <BasicLayout />
+</template>
+
+<script setup lang="ts">
+import BasicLayout from "./layout/BasicLayout.vue";
+</script>
+
+<style scoped lang="less"></style>
