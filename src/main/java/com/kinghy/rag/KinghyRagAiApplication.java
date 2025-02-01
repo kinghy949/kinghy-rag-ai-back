7cc490e538460768f656c80f805a3bdf7b94bb09
kinghy <470527375@qq.com>
2025-02-01 18:35:50 +0800

    Initial commit

---
+++ src/main/java/com/kinghy/rag/KinghyRagAiApplication.java
@@ -0,0 +1,13 @@
+package com.kinghy.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinghyRagAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinghyRagAiApplication.class, args);
+    }
+
+}
